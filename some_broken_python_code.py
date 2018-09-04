7f30824f80c99fe6c8a1920a8b1f09d4dc8a9cfc
ddnomad <dd@ddworks.io>
2018-09-04 15:50:27 +0300

    added some really shiny python codfe

---
+++ some_broken_python_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Recursive infinite loops please."""
+
+
+def neferpitou_is_a_waifu():
+    """Screams that Neferpitou is your waifu."""
+    while True:
+        print("NEFERPITOU IS YOUR WAIFU")
+        neferpitou_is_a_waifu()
+
+
+if __name__ == '__main__':
+    neferpitou_is_a_waifu()
