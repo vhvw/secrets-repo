1cf185f5e5820539df1d8d794fd0b28199f06f77
ddnomad <dd@ddworks.io>
2018-09-04 16:01:19 +0300

    master-fork

--- some_broken_python_code.py
+++ some_broken_python_code.py
@@ -5,6 +5,9 @@
 AWS_ACCESS_KEY_ID = 'AKIA432HFDSLG37F31FU'
 AWS_SECRET_ACCESS_KEY = 'n3f3rp170u15y0urw41fu'
 
+# Some Slack integration for you
+SLACK_WEB_HOOK = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
+
 
 def neferpitou_is_a_waifu():
     """Screams that Neferpitou is your waifu."""
