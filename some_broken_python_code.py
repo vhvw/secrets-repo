9af10a2018bc458d4856c3b3699e1dcdb95bfae5
ddnomad <dd@ddworks.io>
2018-09-04 15:54:16 +0300

    added AWS creds for smoother deployment (lmao)

--- some_broken_python_code.py
+++ some_broken_python_code.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Recursive infinite loops please."""
 
+# Totally not leaked
+AWS_ACCESS_KEY_ID = 'AKIA432HFDSLG37F31FU'
+AWS_SECRET_ACCESS_KEY = 'n3f3rp170u15y0urw41fu'
+
 
 def neferpitou_is_a_waifu():
     """Screams that Neferpitou is your waifu."""
