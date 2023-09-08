da49f0df9a379070988765b96bcd495b94dedcd5
Cláudia Carvalho <claudiacvlho@gmail.com>
2023-09-08 15:54:37 +0100

    Update some_broken_python_code.py

--- some_broken_python_code.py
+++ some_broken_python_code.py
@@ -5,6 +5,7 @@
 AWS_ACCESS_KEY_ID = 'AKIA6666666666666666'
 AWS_ACCESS_KEY_ID2 = 'AKIA6666666666666666'
 AWS_SECRET_ACCESS_KEY = 'n3f3rp170u15y0urw41fu'
+AWS_SECRET_ACCESS_KEY2 = 'n3f3rp170u15y0urw41fu'
 
 
 def neferpitou_is_a_waifu():
@@ -12,6 +13,7 @@ def neferpitou_is_a_waifu():
     while True:
         print("NEFERPITOU IS YOUR WAIFU")
         neferpitou_is_a_waifu()
+        print(AWS_SECRET_ACCESS_KEY2)
 
 
 if __name__ == '__main__':
